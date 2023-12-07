611cfb99fd1e52b29bb9b60c6184518690b578df
sychonix <110755590+sychonix@users.noreply.github.com>
2023-12-07 21:45:19 +0700

    Update Faucet.tsx

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -39,11 +39,9 @@ export default function Faucet() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="sychonix.jpeg" alt="Testnet Faucet" />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Testnet Faucet</h2>
+            <img className="mx-auto h-12 w-auto" src="sychonix.jpeg" alt="Sychonix Faucet" />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sychonix Faucet</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
-              <a href="https://github.com/orgs/0xDeploy/repositories" target="_blank" rel="noreferrer" className="font-medium text-indigo-600 hover:text-indigo-500">
-                Open Source
               </a>
             </p>
           </div>
