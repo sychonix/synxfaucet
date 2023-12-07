3fbf2b8c534b92c674e20eca011ac89225cd70e6
sychonix <110755590+sychonix@users.noreply.github.com>
2023-12-07 21:47:20 +0700

    Update Faucet.tsx

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -36,14 +36,10 @@ export default function Faucet() {
 
   return (
     <>
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
+      <div className=" flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className=" w-full max-w-md space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="sychonix.jpeg" alt="Sychonix Faucet" />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sychonix Faucet</h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              </a>
-            </p>
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div className="-space-y-px rounded-md shadow-sm">
@@ -59,6 +55,11 @@ export default function Faucet() {
                 Request SYNX Token
               </button>
             </div>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              <a href="https://twitter.com/sychonixx" target="_blank" rel="noreferrer" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Follow me on X
+              </a>
+            </p>
           </form>
         </div>
       </div>
